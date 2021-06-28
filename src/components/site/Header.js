1f68a3b4e08bbaa04db16ba42e2aee6b33d5c4b4
Miguel Caballero <cp0251963@gmail.com>
2021-06-28 17:55:18 -0400

    Beginner React  project

---
+++ src/components/site/Header.js
@@ -0,0 +1,20 @@
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar className="header">
+        <NavbarBrand href="'/">React Library</NavbarBrand>
+        <Nav className="ml-auto">
+          <NavItem>
+            <NavLink href="https://github.com/yourhandle/yourRepoForThisApp">
+              GitHub
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
