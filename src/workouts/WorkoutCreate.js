079db99731a509f26de02c81cf52f25ba33ecfce
Miguel Caballero <cp0251963@gmail.com>
2021-06-29 22:50:07 -0400

    updated

--- src/workouts/WorkoutCreate.js
+++ src/workouts/WorkoutCreate.js
@@ -6,7 +6,69 @@ const WorkoutCreate = (props) => {
   const [definition, setDefinition] = useState('');
   const [result, setResult] = useState('');
 
-  return <p>WorkoutCreate Component</p>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:8080/log', {
+      method: 'POST',
+      body: JSON.stringify({
+        log: {
+          description: description,
+          definition: definition,
+          result: result,
+        },
+      }),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer${props.token}`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((logData) => {
+        console.log(logData);
+        setDescription('');
+        setDefinition('');
+        setResult('');
+        props.fetchWorkouts();
+      });
+  };
+
+  return (
+    <>
+      <h3>Log a Workout</h3>
+      <Form>
+        <FormGroup>
+          <Label htmlFor="description"></Label>
+          <Input
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="definition"></Label>
+          <Input
+            type="select"
+            name="definition"
+            value={definition}
+            onChange={(e) => setDefinition(e.target.value)}
+          >
+            <option value="Time">Time</option>
+            <option value="Weight">Weight</option>
+            <option value="Distance">Distance</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="result"></Label>
+          <Input
+            name="result"
+            value={result}
+            onChange={(e) => setResult(e.target.value)}
+          ></Input>
+        </FormGroup>
+        <Button type="submit">Click to Submit</Button>
+      </Form>
+    </>
+  );
 };
 
 export default WorkoutCreate;
--- src/workouts/WorkoutIndex.js
+++ src/workouts/WorkoutIndex.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import WorkoutCreate from './WorkoutCreate';
+import WorkoutTable from './WorkoutTable';
+import WorkoutEdit from './WorkoutEdit';
 
 const WorkoutIndex = (props) => {
   const [workouts, setWorkouts] = useState([]);
   const fetchWorkouts = () => {
-    fetch('http://localhost:3000/log', {
+    fetch('http://localhost:8080/workoutlog', {
       method: 'GET',
       headers: new Headers({
         'Content-Type': 'application/json',
-        Authorization: `Bearer${props.token}`,
+        Authorization: `Bearer ${props.token}`,
       }),
     })
       .then((res) => res.json())
@@ -33,9 +35,11 @@ const WorkoutIndex = (props) => {
           ></WorkoutCreate>
         </Col>
         <Col md="9">
-          <h2>
-            Log a workout to see a table. This will be added in later pages
-          </h2>
+          <WorkoutTable
+            workouts={workouts}
+            fetchWorkouts={fetchWorkouts}
+            token={props.token}
+          ></WorkoutTable>
         </Col>
       </Row>
     </Container>
